e860fa00da663c0d8e8bcb3be35e07eac7a49ecc
Damien <damien.philippe@edu.esiee.fr>
2024-06-12 18:20:13 +0200

    We don't need this file anymore

--- SQLScripts/bestClient.sql
+++
@@ -1,6 +0,0 @@
-SELECT c.idClient, c.nomClient, c.prenomClient, SUM(co.prixCommande) AS montantTotal
-FROM Clients c
-JOIN Commandes co ON c.idClient = co.idClient
-GROUP BY c.idClient, c.nomClient, c.prenomClient
-ORDER BY montantTotal DESC
-LIMIT 5;
